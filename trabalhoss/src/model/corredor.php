a5126ec7589b4d43c6208a9028fd9b5a15fc577a
oi <oi>
2023-08-31 21:54:08 -0300

    t

---
+++ trabalhoss/src/model/corredor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\model;
+
+class corredor {
+    
+    public $nome;
+   
+    function __construct($nome){
+       
+            $this->nome =$nome;
+
+        }
+    }
+
+
+
+
+
+?>
